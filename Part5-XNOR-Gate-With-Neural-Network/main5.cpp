c108dbd1820d1b7e356058003fafb7e1dcb293ad
Joe DiVita <joediv31@gmail.com>
2013-06-26 09:09:48 -0500

    update part5

--- Part5-XNOR-Gate-With-Neural-Network/main5.cpp
+++ Part5-XNOR-Gate-With-Neural-Network/main5.cpp
@@ -19,18 +19,16 @@ void addBias(mat &inputs)
     inputs.insert_rows(0, inputBias);
 }
 
-mat reduceNoise(mat &value)
+void reduceNoise(mat &value)
 {
     //clean up the noise a bit
     if(value(0,0) < 0.05)
         value(0,0) = 0.0;
     if(value(0,0) > 0.95)
         value(0,0) = 1.0;
-    
-    return value;
 }
 
-mat AND_Gate(mat &inputs)
+mat AND_Gate(const mat &inputs)
 {
     mat theta_AND = mat(1, inputs.n_rows);
     theta_AND(0,0) = -30.0;
@@ -38,12 +36,12 @@ mat AND_Gate(mat &inputs)
     for(int i=1; i<(inputs.n_rows); i++)
         theta_AND(0,i) = 20.0;
     
-    mat AND_result = sigmoid(theta_AND*inputs);
+    mat AND_result = reduceNoise(sigmoid(theta_AND*inputs));
     
-    return reduceNoise(AND_result);
+    return AND_result;
 }
 
-mat OR_Gate(mat &inputs)
+mat OR_Gate(const mat &inputs)
 {
     mat theta_OR = mat(1, inputs.n_rows);
     theta_OR(0,0) = -10.0;
@@ -51,12 +49,12 @@ mat OR_Gate(mat &inputs)
     for(int i=1; i<(inputs.n_rows); i++)
         theta_OR(0,i) = 20.0;
     
-    mat OR_result = sigmoid(theta_OR*inputs);
+    mat OR_result = reduceNoise(sigmoid(theta_OR*inputs));
     
-    return reduceNoise(OR_result);
+    return OR_result;
 }
 
-mat NOT_AND_NOT_Gate(mat &inputs)
+mat NOT_AND_NOT_Gate(const mat &inputs)
 {
     mat theta_NOT_AND_NOT = mat(1, inputs.n_rows);
     theta_NOT_AND_NOT(0,0) = 10.0;
@@ -64,9 +62,9 @@ mat NOT_AND_NOT_Gate(mat &inputs)
     for(int i=1; i<(inputs.n_rows); i++)
         theta_NOT_AND_NOT(0,i) = -20.0;
     
-    mat NOT_AND_NOT_result = sigmoid(theta_NOT_AND_NOT*inputs);
+    mat NOT_AND_NOT_result = reduceNoise(sigmoid(theta_NOT_AND_NOT*inputs));
     
-    return reduceNoise(NOT_AND_NOT_result);
+    return NOT_AND_NOT_result;
 }
 
 int main()
@@ -92,3 +90,4 @@ int main()
     
     return 0;
 }
+
